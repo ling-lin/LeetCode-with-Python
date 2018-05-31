7fac37ff80fa46ee719755750df14b1e7f6ca956
linling <linling290@ruc.edu.cn>
2018-06-01 00:16:38 +0800

    modify

--- 766_toeplitz-matrix.py
+++ 766_toeplitz-matrix.py
@@ -4,9 +4,12 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        for i in range(len(matrix)-1):
-            for j in range(len(matrix[0])-1):
-                if matrix[i][j] <> matrix[i+1][j+1]:
-                    return False
-                else:
-                    return True
+        if len(matrix) > 1 and len(matrix[0]) > 1:
+            cnt = 0
+            for i in range(len(matrix)-1):
+                for j in range(len(matrix[i])-1):
+                    if matrix[i][j] <> matrix[i+1][j+1]:
+                        cnt += 1
+            return cnt == 0
+        else:
+            return True
